48716c0bbe4d76f700f39a247811193f73f2ca12
Maksym <72688794+MaksIMMum@users.noreply.github.com>
2026-04-28 16:12:57 +0300

    feat: initialize maze project with Point and Maze models and ignore build artifacts

--- Maze/models.py
+++ Maze/models.py
@@ -17,3 +17,49 @@ class Point:
 
     def __repr__(self):
         return f"({self.x},{self.y})"
+
+
+class Maze:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+        self.grid = [[1 for _ in range(width)] for _ in range(height)]
+
+    def is_in_bounds(self, point: Point) -> bool:
+        if not isinstance(point, Point):
+            return False
+            #raise ValueError("is_in_bounds expects a Point class")
+        return 0 <= point.x < self.height and 0 <= point.y < self.width
+        #return IndexError("Point out of bounds")
+
+
+    def is_wall(self, point: Point) -> bool:
+        if self.is_in_bounds(point):
+            return self.grid[point.x][point.y] == 1
+        return True
+    def set_passable(self, point: Point):
+        if self.is_in_bounds(point):
+            self.grid[point.x][point.y] = 0
+
+    def get_passable_neighbours(self, point: Point) -> list[Point]:
+        passable = []
+        x = point.x
+        y = point.y
+        for xr, yr in [(-1,0), (1,0), (0,1), (0,-1)]:
+            n_p = Point(xr+x, yr+y)
+            if not self.is_wall(n_p):
+                passable.append(n_p)
+        return passable
+
+
+    def __str__(self):
+        res = []
+        for row in self.grid:
+            row_str = "".join(["██" if cell == 1 else "  " for cell in row])
+            res.append(row_str)
+        return "\n".join(res)
+
+if __name__ == "__main__":
+    m = Maze(10, 10)
+    print(m)
