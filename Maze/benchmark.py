68b333665eff5bd82175c4dbcbc0c4239b3eb9fe
Maksym <72688794+MaksIMMum@users.noreply.github.com>
2026-04-28 18:30:14 +0300

    feat: implement backtracking, dead-end filling, and heuristic-based maze solvers with benchmark support

--- Maze/benchmark.py
+++ Maze/benchmark.py
@@ -3,7 +3,7 @@ import sys
 import time
 from models import Maze, Point
 from generator import MazeGenerator
-from solvers import MazeSolver, OptimizedMazeSolver
+from solvers import MazeSolver, OptimizedMazeSolver, SmartMazeSolver
 
 if __name__ == "__main__":
     sys.setrecursionlimit(100_000)
@@ -13,17 +13,14 @@ if __name__ == "__main__":
     original_maze = generator.generate()
 
     # --- СТВОРЮЄМО ЦИКЛИ ---
-    walls_to_break = 200
+    walls_to_break = 1000
     print(f"Ламаємо {walls_to_break} стін для створення циклів...")
     original_maze.break_random_walls(walls_to_break)
 
     # Тепер копіюємо "зіпсований" лабіринт для обох алгоритмів
     maze_for_classic = copy.deepcopy(original_maze)
     maze_for_optimized = copy.deepcopy(original_maze)
-
-
-    maze_for_classic = copy.deepcopy(original_maze)
-    maze_for_optimized = copy.deepcopy(original_maze)
+    maze_for_smart = copy.deepcopy(original_maze)
 
     # --- 1. Класичний алгоритм ---
     classic_solver = MazeSolver(maze_for_classic)
@@ -50,3 +47,16 @@ if __name__ == "__main__":
     print("\n--- Запуск оптимізованого алгоритму (Dead-end filling) ---")
     print(f"Відвідано клітинок (під час пошуку): {len(opt_solver.visited)}")
     print(f"Загальний час виконання (з обробкою): {time_opt_ms:.4f} мс")
+
+    # 3
+
+    sm_solver = SmartMazeSolver(maze_for_smart)
+
+    start_sm = time.perf_counter()
+    sm_solver.solve()
+    end_sm = time.perf_counter()
+
+    time_sm_ms = (end_sm - start_sm) * 1000
+    print("\n--- Запуск оптимізованого алгоритму (MRV) ---")
+    print(f"Відвідано клітинок (під час пошуку): {len(sm_solver.visited)}")
+    print(f"Загальний час виконання (з обробкою): {time_sm_ms:.4f} мс")
--- Maze/solvers.py
+++ Maze/solvers.py
@@ -55,22 +55,46 @@ class OptimizedMazeSolver(MazeSolver):
         return super().solve()
 
 
-if __name__ == "__main__":
-    import sys
 
-    sys.setrecursionlimit(100_000)
-    generator = MazeGenerator(41, 41)
-    my_maze = generator.generate()
-
-    solver = MazeSolver(my_maze)
-    solver.solve()
-    print(my_maze.__str__(path=solver.path))
-    print(f"\nFinal Path Distance unoptimized: {len(solver.path)} steps")
-    print(f"Total Explored Cells1: {len(solver.visited)}")
+class SmartMazeSolver(MazeSolver):
+    def _get_ordered_neighbours(self, point: Point) -> list[Point]:
+        passable = self.maze.get_passable_neighbours(point)
+        end = self.maze.end_point
+        return sorted(passable, key=lambda p: abs(p.x - end.x) + abs(p.y - end.y))
 
 
-    solver2 = OptimizedMazeSolver(my_maze)
-    solver2.solve()
-    print(my_maze.__str__(path=solver2.path))
-    print(f"\nFinal Path Distance deadend optimization: {len(solver2.path)} steps")
-    print(f"Total Explored Cells2: {len(solver2.visited)}")
+    def _backtrack(self, current_point: Point) -> bool:
+        if current_point == self.maze.end_point:
+            self.path.append(current_point)
+            return True
+        self.visited.add(current_point)
+        self.path.append(current_point)
+        cur_neigh = self._get_ordered_neighbours(current_point)
+
+        for neigh in cur_neigh:
+            if neigh not in self.visited:
+                res = self._backtrack(neigh)
+                if res:
+                    return True
+        self.path.pop()
+        return False
+
+# if __name__ == "__main__":
+#     import sys
+
+#     sys.setrecursionlimit(100_000)
+#     generator = MazeGenerator(41, 41)
+#     my_maze = generator.generate()
+
+#     solver = MazeSolver(my_maze)
+#     solver.solve()
+#     print(my_maze.__str__(path=solver.path))
+#     print(f"\nFinal Path Distance unoptimized: {len(solver.path)} steps")
+#     print(f"Total Explored Cells1: {len(solver.visited)}")
+
+
+#     solver2 = OptimizedMazeSolver(my_maze)
+#     solver2.solve()
+#     print(my_maze.__str__(path=solver2.path))
+#     print(f"\nFinal Path Distance deadend optimization: {len(solver2.path)} steps")
+#     print(f"Total Explored Cells2: {len(solver2.visited)}")
